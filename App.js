aa909e72146529346d659d3f98b21088364ea28e
AJAberdein <ashley@aberdein.co.za>
2019-08-29 17:51:26 +0200

    connect with API

--- App.js
+++ App.js
@@ -1,43 +1,263 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import {
+    Button,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    TouchableOpacity,
+    ScrollView,
+    Platform,
+    Image
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-import Draw from './components/Draw';
+// import Modal from 'react-native-modalbox';
 
+// import Draw from './components/Draw';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas';
+import { SketchCanvas } from '@terrylinla/react-native-sketch-canvas';
 
-type
-Props = {};
-export default class App extends Component<Props> {
+export default class app extends Component {
 
-    getDrawingHandler = () => {
-        // alert('hi');
-        console.log('pressed!!!')
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            example: 0,
+            page: 'home',
+            color: 'black',
+            thickness: 10,
+            message: '',
+            scrollEnabled: true,
+            predictor: null,
+            prediction: null,
+            is_match: 'closed'
+        }
+        this.getPredictorClass();
     }
 
+
+    getPredictorClass()
+    {
+        fetch('http://127.0.0.1:8000/class', {
+            method: 'GET',
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({predictor: responseJson.class})
+            })
+            .catch((error) =>{
+                console.error(error);
+            });
+    }
 
     render() {
         return (
             <View style={styles.container}>
-                {/*<Text style={styles.welcome}>Welcome to React Native!</Text>*/}
-                {/*<Text style={styles.welcome}>Hello, World!</Text>*/}
-                {/*<Text style={styles.instructions}>To get started, edit App.js</Text>*/}
-                {/*<Text style={styles.instructions}>{instructions}</Text>*/}
+                {
+                    this.state.page === 'home' &&
+                    <View style={{ justifyContent: 'center', alignItems: 'center', width: 340 }}>
+                        <TouchableOpacity onPress={() => {
+                            this.setState({ page: 'draw' })
+                        }}>
 
-                {/*<Draw onDrawingFinished={this.getDrawingHandler}/>*/}
-                <Draw/>
+                            <Image
+                                source={require('./components/img/draw-animals-icon.png')}
+                                style="{styles.icon}"
+                            />
+
+                            {/*<Text style={{ alignSelf: 'center', marginTop: 15, fontSize: 18 }}>- DRAW PREDICT -</Text>*/}
+                            <Text style={{ color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18, fontFamily: 'Schoolbell'  }}>Test your Animal Drawing Skills!</Text>
+                            <Text style={{ color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18 }}></Text>
+
+
+                            <Image
+                                source={require('./components/img/draw-animals-start.png')}
+                                style="{styles.icon}"
+                            />
+
+
+
+                        </TouchableOpacity>
+
+                    </View>
+                }
+
+                {
+                    this.state.page === 'draw' &&
+                    <View style={{ flex: 1, flexDirection: 'row' }}>
+
+
+
+                        <View style={{flex: 1, flexDirection: 'row'}}>
+                            <View style={{flex: 1, flexDirection: 'column'}}>
+
+                                <Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18}}></Text>
+                                <Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18}}></Text>
+                                {/*<Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18, fontFamily: 'Schoolbell' }}>Let's Draw*/}
+                                <Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 22, fontFamily: 'Schoolbell'  }}>Let's Draw
+                                    a {this.state.predictor}</Text>
+
+                                <SketchCanvas
+                                    localSourceImage={{filename: 'whale.png', directory: SketchCanvas.MAIN_BUNDLE, mode: 'AspectFit'}}
+                                    // localSourceImage={{ filename: 'bulb.png', directory: RNSketchCanvas.MAIN_BUNDLE }}
+                                    ref={ref => this.canvas = ref}
+                                    style={{flex: 1}}
+                                    strokeColor={this.state.color}
+                                    strokeWidth={this.state.thickness}
+                                    onStrokeStart={(x, y) => {
+                                    }}
+                                    onStrokeChanged={(x, y) => {
+                                    }}
+                                    //onStrokeEnd={props.onGetResults}
+                                    onStrokeEnd={() => {
+                                        this.canvas.getBase64('jpg', false, true, false, true, (err, result) => {
+                                            fetch('http://127.0.0.1:8000/draw-predict', {
+                                                method: 'POST',
+                                                headers: {
+                                                    'Accept': 'application/json',
+                                                    'Content-Type': 'application/json',
+                                                },
+                                                body: JSON.stringify({
+                                                    image_str: result,
+                                                    prediction: this.state.predictor
+                                                }),
+                                            }).then((response) => response.json()).then((responseJson) => {
+                                                if(this.state.predictor == responseJson.category) {
+                                                    //{/*alert('match')*/}
+                                                    if(this.state.is_match != 'open') {
+                                                        this.state.is_match = 'open';
+                                                    }
+                                                }
+
+                                                this.setState({prediction: responseJson.category});
+
+                                            }).catch((error) =>{
+                                                console.error(error);
+                                            });
+
+
+                                        })
+                                    }}
+
+                                    onPathsChange={(pathsCount) => {
+                                       // console.log('pathsCount', pathsCount)
+                                    }}
+
+                                />
+
+
+
+
+                                {
+                                    this.state.is_match == 'open' &&
+                                    <View style={{
+                                        position: 'absolute',
+                                        alignItems: 'center',
+                                        top: '30%',
+                                        width: 300,
+                                        left: 40,
+                                        textAlign: 'center',
+                                        backgroundColor: '#f39c12',
+                                        color: "#ffffff",
+                                        borderRadius: 5,
+                                    }}>
+
+                                        <Text style={{
+                                            color: '#ffffff',
+                                            alignSelf: 'center',
+                                            marginTop: 15,
+                                            fontSize: 22,
+                                            fontFamily: 'Schoolbell',
+                                            textAlign: 'center'
+                                        }}>
+                                            Congratulations!
+                                            {"\n"}  You are an Ace at
+                                            {"\n"} drawing {this.state.predictor}s!
+                                        </Text>
+
+
+                                        <TouchableOpacity style={[styles.functionButton, {backgroundColor: '#e67e22', width: 150}]}
+                                                          onPress={() => {
+
+
+                                                              this.getPredictorClass();
+                                                              this.canvas.clear()
+                                                              this.setState({ prediction: null });
+
+                                                              this.setState({ is_match: 'closed' });
+
+
+
+                                                          }}>
+                                            <Text style={{color: 'white', fontSize: 18, fontFamily: 'Schoolbell'}}>Let's try another</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                }
+
+
+
+
+
+                                {
+                                    this.state.prediction &&
+                                    <View style={{
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                        marginBottom: 30,
+                                        marginLeft: 100,
+                                        textAlign: 'center'
+                                    }}>
+
+                                        <Text style={{
+                                            color: '#e67e22',
+                                            alignSelf: 'center',
+                                            marginTop: 15,
+                                            fontSize: 22,
+                                            fontFamily: 'Schoolbell',
+                                            textAlign: 'center'
+                                        }}>It looks like
+                                            a {this.state.prediction}</Text>
+                                    </View>
+                                }
+
+
+                                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 100}}>
+
+
+                                    <Text style={{marginRight: 8, fontSize: 20}}>{this.state.message}</Text>
+                                    <TouchableOpacity style={[styles.functionButton, {backgroundColor: '#e67e22', width: 150}]}
+                                                      onPress={() => {
+                                                          this.canvas.clear()
+
+                                                      }}>
+                                        <Text style={{color: 'white', fontSize: 18, fontFamily: 'Schoolbell'}}>Clear</Text>
+                                    </TouchableOpacity>
+
+
+                                    <Text style={{marginRight: 8, fontSize: 20}}>{this.state.message}</Text>
+                                    <TouchableOpacity style={[styles.functionButton, {backgroundColor: '#e67e22', width: 150,  marginRight: 20}]}
+                                                      onPress={() => {
+                                                          this.canvas.clear()
+                                                          this.getPredictorClass()
+                                                          this.state.prediction = undefined
+
+                                                      }}>
+                                        <Text style={{color: 'white', fontSize: 18, fontFamily: 'Schoolbell'}}>Try Another</Text>
+                                    </TouchableOpacity>
+
+                                </View>
+
+
+                            </View>
+                        </View>
+
+                    </View>
+                }
             </View>
         );
     }
@@ -45,20 +265,73 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
     container: {
-        top: 30,
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    // welcome: {
-    //   fontSize: 20,
-    //   textAlign: 'center',
-    //   margin: 10,
-    // },
-    // instructions: {
-    //   textAlign: 'center',
-    //   color: '#333333',
-    //   marginBottom: 5,
-    // },
+    icon: {
+        height: '30%',
+        width: '30%',
+        position: 'absolute',
+    },
+
+    strokeColorButton: {
+        marginHorizontal: 2.5,
+        marginVertical: 8,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+    },
+    strokeWidthButton: {
+        marginHorizontal: 2.5,
+        marginVertical: 8,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#39579A'
+    },
+    functionButton: {
+        marginHorizontal: 2.5,
+        marginVertical: 8,
+        height: 30,
+        width: 60,
+        backgroundColor: '#39579A',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+        alignSelf: 'stretch'
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20
+    },
+    page: {
+        flex: 1,
+        height: 300,
+        elevation: 2,
+        marginVertical: 8,
+        backgroundColor: 'white',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.75,
+        shadowRadius: 2
+    }
 });
+
+AppRegistry.registerComponent('app', () => app);
--- components/Draw.js
+++ components/Draw.js
@@ -16,134 +16,181 @@ import {SketchCanvas} from '@terrylinla/react-native-sketch-canvas';
 import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas';
 
 
+
 const draw = props => {
 
 
-    strokeChanged = (x, y) => {
-        // alert(x + '; ' + y)
-        alert(JSON.stringify(x))
-        alert()
-        // getPaths()
+    // this.onChange = this.onChange.bind(this)
+
+    state = {
+        test: 'hello',
+        example: 1,
+        color: 'black',
+        thickness: 10,
+        message: '',
+        scrollEnabled: true,
+        predictor: null,
+        prediction: null
     }
 
-    strokeEnded = (stroke) => {
-        // alert(x + '; ' + y)
-        // alert(JSON.stringify(x))
-        alert(this.canvas.getPath())
-        // getPaths()
-    }
+
+    //console.log("props");
+    //console.log(props);
+
+
+
+
+
 
     return (
 
         <View style={{flex: 1, flexDirection: 'row'}}>
-            {/*<SketchCanvas*/}
-            {/*style={{ flex: 1 }}*/}
-            {/*strokeColor={'black'}*/}
-            {/*strokeWidth={7}*/}
-            {/*onStrokeChanged={this.strokeChanged}*/}
-            {/*/>*/}
+            <View style={{flex: 1, flexDirection: 'column'}}>
+
+                <Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18}}></Text>
+                <Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18}}></Text>
+                {/*<Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18, fontFamily: 'Schoolbell' }}>Let's Draw*/}
+                <Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18, fontFamily: 'Schoolbell'  }}>Let's Draw
+                    a {props.predictor}</Text>
+
+                <SketchCanvas
+                    localSourceImage={{filename: 'whale.png', directory: SketchCanvas.MAIN_BUNDLE, mode: 'AspectFit'}}
+                    // localSourceImage={{ filename: 'bulb.png', directory: RNSketchCanvas.MAIN_BUNDLE }}
+                    ref={ref => this.canvas = ref}
+                    style={{flex: 1}}
+                    strokeColor={this.state.color}
+                    strokeWidth={this.state.thickness}
+                    onStrokeStart={(x, y) => {
+                        //console.log('x: ', x, ', y: ', y)
+                        //this.setState({ message: 'Start' })
+                    }}
+                    onStrokeChanged={(x, y) => {
+                        //console.log('x: ', x, ', y: ', y)
+                        //this.setState({ message: 'Changed' })
+
+                    }}
+                    onStrokeEnd={() => {
+                        this.canvas.getBase64('jpg', false, true, false, true, (err, result) => {
+
+                            //console.log(result)
+                            fetch('http://127.0.0.1:8000/draw-predict', {
+                                method: 'POST',
+                                headers: {
+                                    'Accept': 'application/json',
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify({
+                                    image_str: result,
+                                    prediction: props.predictor
+                                }),
+                            }).then((response) => response.json()).then((responseJson) => {
+
+                                console.log('target: ', props.predictor, 'prediction: ', responseJson);
+
+                                this.state.prediction = responseJson.category;
+
+                            }).catch((error) =>{
+                                console.error(error);
+                            });
 
 
-            <RNSketchCanvas
-                ref={ref => this.canvas = ref}
-                containerStyle={{backgroundColor: 'transparent', flex: 1}}
-                canvasStyle={{backgroundColor: 'transparent', flex: 1}}
-                defaultStrokeIndex={0}
-                defaultStrokeWidth={5}
+                        })
+                    }}
 
-                savePreference={() => {
-                    return {
-                        folder: 'RNSketchCanvas',
-                        filename: String(Math.ceil(Math.random() * 100000000)),
-                        transparent: false,
-                        imageType: 'png'
-                    }
-                }}
+                    onPathsChange={(pathsCount) => {
+                        console.log('pathsCount', pathsCount)
+                    }}
 
-                clearComponent={
-                    <View style={styles.functionButton}><Text style={{color: 'white'}}>Clear</Text></View>
-                }
-                saveComponent={
-                    <View style={styles.functionButton}><Text style={{color: 'white'}}>Save</Text></View>
-                }
-                onStrokeEnd={(id) => {
-                    //Alert.alert(JSON.stringify(this.canvas))
+                    //clearComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Clear</Text></View>}
 
+                />
 
-                    //Alert.alert(JSON.stringify(this.canvas.getPaths()))
-                    //Alert.alert(JSON.stringify(this.canvas.getPaths()))
-                    //this.canvas.getBase64('jpg', false, true, true, (err, result) => {
-                        //console.log(result)
-                        //Alert.alert(JSON.stringify(result))
-                    //})
-                    //alert(JSON.stringify(this.canvas.getPaths()))
-                    //alert(JSON.stringify(this.canvas))
-                    //alert(this.canvas.getPaths())
-                }}
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 100, marginLeft: 100, textAlign: 'center'}}>
 
+                    <Text style={{color: '#e67e22', alignSelf: 'center', marginTop: 15, fontSize: 18, fontFamily: 'Schoolbell' , textAlign: 'center' }}>It looks like
+                        a {state.prediction}</Text>
 
+                </View>
 
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 100}}>
 
+                    <Text style={{marginRight: 8, fontSize: 20}}>{this.state.message}</Text>
+                    <TouchableOpacity style={[styles.functionButton, {backgroundColor: 'black', width: 90}]}
+                                      onPress={() => {
+                                          //console.log(this.canvas.getPaths())
+                                          //Alert.alert(JSON.stringify(this.canvas.getPaths()))
+                                          this.canvas.clear()
 
-            />
-
-
-
-
-
+                                      }}>
+                        <Text style={{color: 'white'}}>Clear</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
 
-
-
-
-
-        // <SketchCanvas/>
-        // <Button title="Submit" onPress={props.onDrawingFinished}/>
     );
 };
 
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF',
-//     },
-// });
-
-
 const styles = StyleSheet.create({
-    // container: {
-    //     flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF',
-    // },
-    // strokeColorButton: {
-    //     marginHorizontal: 2.5,
-    //     marginVertical: 8,
-    //     width: 30,
-    //     height: 30,
-    //     borderRadius: 15,
-    // },
-    // strokeWidthButton: {
-    //     marginHorizontal: 2.5,
-    //     marginVertical: 8,
-    //     width: 30,
-    //     height: 30,
-    //     borderRadius: 15,
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     backgroundColor: '#39579A'
-    // },
+
+    strokeColorButton: {
+        marginHorizontal: 2.5,
+        marginVertical: 8,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+    },
+    strokeWidthButton: {
+        marginHorizontal: 2.5,
+        marginVertical: 8,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#39579A'
+    },
     functionButton: {
-        // top:10,
         marginHorizontal: 2.5,
         marginVertical: 8,
         height: 30,
-        width: 150,
+        width: 60,
         backgroundColor: '#39579A',
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 5,
+    },
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+        alignSelf: 'stretch'
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20
+    },
+    page: {
+        flex: 1,
+        height: 300,
+        elevation: 2,
+        marginVertical: 8,
+        backgroundColor: 'white',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.75,
+        shadowRadius: 2
     }
 });
 
 export default draw;
 
-// AppRegistry.registerComponent('draw', () => draw);
